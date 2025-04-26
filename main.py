7c5b5bbf9970cb9968cb9cab79774756a444c531
carlosq-mv <cquiroz7869@gmail.com>
2025-04-25 23:46:46 -0700

    refactoring

--- main.py
+++ main.py
@@ -1,128 +1,28 @@
-from fastapi import FastAPI, HTTPException
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
 from dotenv import load_dotenv
-import os, httpx, redis, json
+import os
 
-from schemas.book import Book
-from schemas.search import SearchItem
+from routers.search_api import s_api
+from routers.book_api import b_api
+from lib.mongo import DBClient
 
 load_dotenv()
 
-app = FastAPI()
-
-# NOTE: redis stores data as bytes so use 'json.dumps()' when storing and 'json.loads()' when retrieving
-# default to 'redis' if 'localhost' doesn't work in docker container
-redis_client = redis.Redis(host=os.getenv("REDIS_HOST", "localhost"), port=6379, db=0, decode_responses=True)
-
-BASE_URL = os.getenv("BASE_URL") 
-API_KEY = os.getenv("BOOK_API")
-
-@app.get("/search")
-async def search(bookname: str, max_results: int = 15, start_index: int = 0):
-    # Check if the search term is empty
-    if not bookname.strip():
-        raise HTTPException(status_code=400, detail="Search item cannot be empty.")
-
-    url = f"{BASE_URL}?q={bookname}&maxResults={max_results}&startIndex={start_index}&key={API_KEY}&printType=books&langRestrict=en"
-    cache_key = f"books_{bookname}_{max_results}_{start_index}"
-    cached_res = redis_client.get(cache_key)
-
-    if cached_res:
-        return {"book query": bookname, "cached": True, "data": json.loads(cached_res)}
-
-    try:
-        # Make an asynchronous request to the Google Books API
-        async with httpx.AsyncClient() as client:
-            response = await client.get(url)
-
-            # If the response status code is not 200 (OK), raise an error
-            response.raise_for_status()
-
-            # Parse the JSON response
-            data = response.json()
-
-            # If no books are found, return an error message
-            if "items" not in data:
-                raise HTTPException(status_code=404, detail="No books found.")
-
-            books = []
-            for item in data.get('items', []):
-                # Second API call to get higher res cover images 
-                imgs_url = f"{BASE_URL}/{item.get('id')}"
-                res = await client.get(imgs_url)
-                res.raise_for_status()
-                d = res.json()
-
-                volume_info = item.get("volumeInfo", {})
-
-                # Get the links to the cover
-                cover_img_list = list(d['volumeInfo'].get("imageLinks", {}).values())
-
-                # Get the ISBN's to the book
-                isbn_list = list(volume_info.get("industryIdentifiers", []))
-
-                # Description
-                desc = d['volumeInfo'].get("description")
-                print(desc)
-
-                # Create Book object
-                book_data = Book(
-                    id=item.get("id"),
-                    title=volume_info.get("title", "Unknown Title"),
-                    description=desc,
-                    page_count=volume_info.get("pageCount"),
-                    average_rating=volume_info.get("averageRating"),
-                    language=volume_info.get("language"),
-                    authors=volume_info.get("authors", []),
-                    genre=volume_info.get("categories", []),
-                    cover_img=cover_img_list,
-                    isbn=isbn_list
-                )
-                if book_data.description is None:
-                    continue
-                books.append(book_data)
-        redis_client.setex(cache_key, 600, json.dumps([book.dict() for book in books]))
-
-        return {"book query": bookname, "cached": False, "data": books}
-
-    except httpx.RequestError as e:
-        # Handle errors that occur during the HTTP request
-        raise HTTPException(status_code=500, detail=f"An error occurred while requesting the Google Books API: {e}")
-
-    except httpx.HTTPStatusError as e:
-        # Handle HTTP errors (e.g., 4xx, 5xx responses)
-        raise HTTPException(status_code=e.response.status_code, detail=f"HTTP error: {e.response.status_code}")
 
 
-@app.post("/recent-searches")
-def post_recent_searches(item: SearchItem):
-    cache_key = "recent_searches"
-    max_len = 5             # Max searches stored
-    ttl_seconds = 86400 * 2     # 2 day (expiration time)
+DB_NAME = os.getenv("DB_NAME")
+MONGO_URI = os.getenv("MONGO_URI")
 
-    # Check if the search term is empty
-    if not item.search_item.strip():
-        raise HTTPException(status_code=400, detail="Search item cannot be empty.")
+mongo = DBClient.get_instance(uri=MONGO_URI, db_name=DB_NAME)
 
-    # Check if the search item is in the cache
-    # if so, remove it and then it will be added to the front of the cache
-    if item.search_item in redis_client.lrange(cache_key, 0, -1):
-        redis_client.lrem(cache_key, 0, item.search_item)
+@asynccontextmanager
+async def lifespan(fapp: FastAPI):
+    yield
+    mongo.close()
 
-    redis_client.lpush(cache_key, item.search_item) # Push to the front of list
-    redis_client.ltrim(cache_key, 0, max_len - 1)   # Make sure only 10 items are kept
-    redis_client.expire(cache_key, ttl_seconds)     # Set expiration 
-
-    recent_searches = redis_client.lrange(cache_key, 0, -1)     # Get list of all items
-    
-    return {"recent_searches": recent_searches, "len" : len(recent_searches)}
+app = FastAPI(lifespan=lifespan)
 
 
-@app.get("/recent-searches")
-def get_recent_searches():
-    cache_key = "recent_searches"
-    max_len = 5            # Max searches stored
-
-    redis_client.ltrim(cache_key, 0, max_len - 1)   # Make sure only 10 items are kept
-    recent_searches = redis_client.lrange(cache_key, 0, -1)     # Get list of all items
-
-    return {"recent_searches": recent_searches, "len" : len(recent_searches)}
+app.include_router(s_api)
+app.include_router(b_api, prefix="/book")
