3bb3c2c63305a664aa201bc72583a30ca82934ec
carlosq-mv <cquiroz7869@gmail.com>
2025-05-23 14:42:38 -0700

    redit client

--- lib/redis.py
+++ lib/redis.py
@@ -2,7 +2,7 @@ import redis, os
 
 # NOTE: redis stores data as bytes so use 'json.dumps()' when storing and 'json.loads()' when retrieving
 # default to 'redis' if 'localhost' doesn't work in docker container
-redis_client = redis.Redis(
+redis_client: redis.Redis = redis.Redis(
     host=os.getenv("REDIS_HOST", "localhost"),
     port=6379, 
     db=0,
