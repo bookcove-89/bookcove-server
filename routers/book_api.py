a68c2fcc95bb2ad7c547c1e95513be7ade4a415c
carlosq-mv <cquiroz7869@gmail.com>
2025-05-24 00:43:31 -0700

    book rest api

--- routers/book_api.py
+++ routers/book_api.py
@@ -3,9 +3,11 @@ from pymongo.errors import PyMongoError
 
 from lib.mongo import DBClient
 from lib.redis import redis_client
+from lib.rabbit import init_rabbit_mq
 from schemas.requests import AddToFavoritesRequest
 from schemas.requests import RemoveFavoriteRequest
 from schemas.book import Book
+import json, logging
 
 b_api = APIRouter()
 
@@ -16,6 +18,17 @@ async def add_to_favorites(request: AddToFavoritesRequest):
         collection = client.db['books']
         request.book.is_favorite = True
         
+        if collection.find_one({ "user_id": request.user_id, "book.id": request.book.id, "book.is_favorite": True}):
+            warning_message = (
+                f"INFO: User '{request.user_id}' attempted to add book "
+                f"(ID: '{request.book.id}') which is already marked as a favorite."
+            )
+            logging.info(warning_message)
+            return {
+                "msg": "Book is already in your favorites.",
+                "is_favorite": True # Reflecting the existing state
+            }
+
         # Create document
         doc = { 
             "user_id": request.user_id, 
@@ -24,6 +37,23 @@ async def add_to_favorites(request: AddToFavoritesRequest):
 
         # Insert into MongoDB
         res = collection.insert_one(doc)
+
+        # RabbitMQ: send message
+        mq_msg_data = {
+            "user_id": request.user_id, 
+            "book": request.book.model_dump(),
+            "action": "added_favorite"
+        }
+
+        try:
+            channel, connection = init_rabbit_mq()
+            channel.queue_declare(queue="book-favorites-queue", durable=True)
+            channel.basic_publish(exchange='', routing_key="book-favorites-queue", body=json.dumps(mq_msg_data))
+        except Exception as mq_err:
+            print(f"Error publishing to RabbitMQ (non-critical): {mq_err}")
+        finally:
+            if connection and connection.is_open:
+                connection.close()
         
         return { "msg" : "success", "inserted_id": str(res.inserted_id) }
     
@@ -33,7 +63,7 @@ async def add_to_favorites(request: AddToFavoritesRequest):
 
     except Exception as e:
         status = e.status_code if e else 500
-        raise HTTPException(status_code=status, detail=f"{e.detail if e else "Internal server error"}")
+        raise HTTPException(status_code=status, detail="Internal server error")
 
 @b_api.patch("/remove-favorite")
 async def remove_favorite(request: RemoveFavoriteRequest):
@@ -43,6 +73,7 @@ async def remove_favorite(request: RemoveFavoriteRequest):
 
         query_filter = { "user_id": request.user_id, "book.id": request.book_id }
         update_data = { "$set": { "book.is_favorite" : request.is_favorite }}
+        book = collection.find_one(query_filter)
 
         res = collection.update_one(query_filter, update_data)
 
@@ -52,6 +83,26 @@ async def remove_favorite(request: RemoveFavoriteRequest):
         if res.modified_count == 0 and res.matched_count > 0:
             # document was found, but the is_favorite status was already set to the requested value
             return {"msg": "Favorite status was already up to date.", "book_id": request.book_id, "is_favorite": request.is_favorite}
+        if book:
+            # RabbitMQ: send message
+            mq_msg_data = {
+                "user_id": request.user_id,
+                "book": book['book'],
+                "action": "remove_favorite"
+            }
+        try:
+            channel, connection = init_rabbit_mq()
+            channel.queue_declare(queue="book-favorites-queue", durable=True)
+            channel.basic_publish(exchange='', routing_key="book-favorites-queue", body=json.dumps(mq_msg_data))
+
+            connection.close()
+        except Exception as mq_err:
+            print(f"Error publishing to RabbitMQ (non-critical): {mq_err}")
+            logging.error(f"Error publishing to RabbitMQ (non-critical): {mq_err}")
+        finally:
+            if connection and connection.is_open:
+                connection.close()
+            
 
         # successfull update
         return {"msg": "Favorite status updated successfully.", "book_id": request.book_id, "is_favorite": request.is_favorite}
@@ -62,18 +113,29 @@ async def remove_favorite(request: RemoveFavoriteRequest):
 
     except Exception as e:
         status = e.status_code if e else 500
-        raise HTTPException(status_code=status, detail=f"{e.detail if e else "Internal server error"}")
+        raise HTTPException(status_code=status, detail="Internal server error")
 
 @b_api.get("/get-favorites", response_model=list[Book])
 async def get_favorites(uid: str):
     cache_key = f"user_{uid}_books"
+
+    # Check if the users favorite books are cached
+    cached_favorites = redis_client.lrange(cache_key, 0, -1)
+    if cached_favorites:
+        cached_response = [Book(**json.loads(book_json_str)) for book_json_str in cached_favorites]
+        return cached_response
+
     try:
         client = DBClient.get_instance()
         collection = client.db['books']
 
         books = []
         for book_doc in collection.find({ "user_id" : uid, "book.is_favorite": True }):
-            books.append(Book(**book_doc['book']))
+            bk = Book(**book_doc['book'])
+            books.append(bk)
+
+            # add to cache
+            redis_client.lpush(cache_key, json.dumps(book_doc['book']))
 
         if not books:
             print(f"No books found for user_id: {uid}")
@@ -82,7 +144,8 @@ async def get_favorites(uid: str):
         return books 
     except PyMongoError as mongo_err:
             print(f"MongoDB error: {mongo_err}")  # Log MongoDB-specific error
+            logging.error(f"MongoDB error: {mongo_err}")
             raise HTTPException(status_code=500, detail="Database error. Please try again later.")
     except Exception as e:
         status = e.status_code if e else 500
-        raise HTTPException(status_code=status, detail=f"{e.detail if e else "Internal server error"}")
+        raise HTTPException(status_code=status, detail="Internal server error")
